5ed4de5ec86c33591e73f5a2b23b767403d7b4bd
KRIPER <jaysmitc@gmail.com>
2025-12-13 06:05:39 +0530

    frontend main-2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useContext } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
